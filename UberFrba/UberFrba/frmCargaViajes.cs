a9ca089d4f6167a85f5ee4f13a8f96dbb7247308
KpdsK <pablo.scalora@gmail.com>
2017-07-10 22:27:58 -0300

    Registro Viajes los horarios se ajustan al rango perteneciente al turno, valida que el inicio sea menor que el final

--- UberFrba/UberFrba/frmCargaViajes.cs
+++ UberFrba/UberFrba/frmCargaViajes.cs
@@ -14,7 +14,6 @@ namespace UberFrba
 {
     public partial class frmRegistroViaje : Form
     {
-
         public string idAuto { get; set; }
         public frmRegistroViaje()
         {
@@ -47,7 +46,6 @@ namespace UberFrba
             }
             return true;
         }
-
    
         private void comboChoferModificacionEnSeleccion(object sender, EventArgs e)
         {
@@ -81,14 +79,32 @@ namespace UberFrba
             frmAutomovilComboTurno.DataSource = tblTurnosDisponibles;
             frmAutomovilComboTurno.DisplayMember = "Turno_Descripcion";
             frmAutomovilComboTurno.ValueMember = "Turno_Id";
-            this.Controls["txtAutomovil"].Text = Convert.ToString(((DataRowView)frmAutomovilComboTurno.SelectedItem)["Auto_Detalle"]);
+            configurarLimitesDeHorarios(frmAutomovilComboTurno);
+            this.txtAutomovil.Text = Convert.ToString(((DataRowView)frmAutomovilComboTurno.SelectedItem)["Auto_Detalle"]);
             this.idAuto = Convert.ToString(((DataRowView)frmAutomovilComboTurno.SelectedItem)["Auto_Id"]);
         }
 
-        private void comboTurno_SelectedIndexChanged(object sender, EventArgs e)
+        private void configurarLimitesDeHorarios(ComboBox frmAutomovilComboTurno)
         {
-            ComboBox frmAutomovilComboTurno = (ComboBox)this.Controls["comboTurno"];
-            this.Controls["txtAutomovil"].Text = Convert.ToString(((DataRowView)frmAutomovilComboTurno.SelectedItem)["Auto_Detalle"]);
+            String horaInicio = Convert.ToString(((DataRowView)frmAutomovilComboTurno.SelectedItem)["Turno_Hora_Inicio"]);
+            String horaFin = Convert.ToString(((DataRowView)frmAutomovilComboTurno.SelectedItem)["Turno_Hora_Fin"]);
+            limpiarLimites(this.selectorDiaHoraInicio);
+            limpiarLimites(this.selectorDiaHoraFin);
+            this.selectorDiaHoraInicio.MinDate = obtenerFechaHora(horaInicio);
+            this.selectorDiaHoraInicio.MaxDate = obtenerFechaHora(horaFin);
+            this.selectorDiaHoraFin.MinDate = obtenerFechaHora(horaInicio);
+            this.selectorDiaHoraFin.MaxDate = obtenerFechaHora(horaFin);
+        }
+
+        private void limpiarLimites(DateTimePicker selectorHorario)
+        {
+            selectorHorario.MinDate = obtenerFechaHora("00");
+            selectorHorario.MaxDate = obtenerFechaHora("24");
+        }
+
+        private static DateTime obtenerFechaHora(String hora)
+        {
+            return DateTime.Parse(DateTime.Now.ToString("dd/MM/yyyy ") + (!hora.Equals("24") ? hora + ":00:00" : "23:59:59"));
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
@@ -98,32 +114,42 @@ namespace UberFrba
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
-            try
+            if (verificarDatosDeFormulario())
             {
-                GD1C2017DataSetTableAdapters.QueriesTableAdapter adaptador =
-                    new GD1C2017DataSetTableAdapters.QueriesTableAdapter();
-                adaptador.registrarViaje(
-                    (int)this.comboChofer.SelectedValue, (int)this.comboCliente.SelectedValue,
-                        Convert.ToInt32(this.idAuto), (int)this.comboTurno.SelectedValue,
-                        Convert.ToDecimal(this.txtCantidadKilometros.Text), (DateTime)this.selectorDiaHoraInicio.Value,
-                        (DateTime)this.selectorDiaHoraFin.Value
-                    );
-                this.Close();
+                try
+                {
+                    GD1C2017DataSetTableAdapters.QueriesTableAdapter adaptador =
+                        new GD1C2017DataSetTableAdapters.QueriesTableAdapter();
+                    adaptador.registrarViaje(
+                        (int)this.comboChofer.SelectedValue, (int)this.comboCliente.SelectedValue,
+                            Convert.ToInt32(this.idAuto), (int)this.comboTurno.SelectedValue,
+                            Convert.ToDecimal(this.txtCantidadKilometros.Text), (DateTime)this.selectorDiaHoraInicio.Value,
+                            (DateTime)this.selectorDiaHoraFin.Value
+                        );
+                    this.Close();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Error en la creacion del viaje. Probablemente este duplicado."
+                            , "Error Creacion de viaje"
+                            , MessageBoxButtons.OK
+                            , MessageBoxIcon.Error);
+                }
             }
-            catch (SqlException ex)
+            else
             {
-                MessageBox.Show("Error en la creacion del viaje. Probablemente este duplicado."
-                        , "Error Creacion de viaje"
-                        , MessageBoxButtons.OK
-                        , MessageBoxIcon.Error);
+                MessageBox.Show("El horario de inicio y fin del viaje debe ser distinto."
+                            , "Error Rango Horario"
+                            , MessageBoxButtons.OK
+                            , MessageBoxIcon.Information);
             }
-
         }
 
         public virtual bool verificarDatosDeFormulario()
         {
             return
-            Validaciones.validarCampoNumerico(this.txtCantidadKilometros.Text);
+            Validaciones.validarCampoNumerico(this.txtCantidadKilometros.Text)
+            && DateTime.Compare(this.selectorDiaHoraInicio.Value, this.selectorDiaHoraFin.Value) < 0;
         }
 
         private void txtCantidadKilometros_KeyPress(object sender, KeyPressEventArgs e)
@@ -131,6 +157,11 @@ namespace UberFrba
             MetodosGlobales.permitirSoloIngresoNumerico(e);
         }
 
-        
+        private void comboTurno_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox frmAutomovilComboTurno = (ComboBox)this.Controls["comboTurno"];
+            this.Controls["txtAutomovil"].Text = Convert.ToString(((DataRowView)frmAutomovilComboTurno.SelectedItem)["Auto_Detalle"]);
+            configurarLimitesDeHorarios(frmAutomovilComboTurno);
+        }
     }
 }
